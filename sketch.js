66d055c868638f43490f823b6cfb5066e66d2ff8
Jiya26 <68414702+Jiya26@users.noreply.github.com>
2020-08-30 21:32:35 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,18 +3,21 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 var engine, world;
+var divsion;
 var ground;
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(480,800);
   engine = Engine.create();
   world = engine.world;  
-  ground=new Ground(300,300,20,20)
+  divsion = new Divisions(200,200,5,100);
+  ground=new Ground(250,700,500,20)
 }
 
 function draw() {
   background(0); 
   Engine.update(engine);
   ground.display();
+  divsion.display();
   drawSprites();
 }
