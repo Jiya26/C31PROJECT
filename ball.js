58996dc9b1f6b668db7ccbc7bbd08273e475f638
Jiya26 <68414702+Jiya26@users.noreply.github.com>
2020-10-10 16:14:58 +0530

    Add files via upload

--- ball.js
+++ ball.js
@@ -6,7 +6,7 @@ class Ball {
           'density':1.0
       }
       this.body = Bodies.circle(x, y, 10, options);
-     
+      this.color = color(random(20,255),random(52,255),random(20,200));
       this.radius = 10;
       
       World.add(world, this.body);
@@ -14,12 +14,11 @@ class Ball {
     display(){
       var pos =this.body.position;
       
-      this.color = color(random(0.255),random(0.255));
       
       var angle = this.body.angle;
       push();
       translate(pos.x, pos.y);
-      fill("white");
+      fill(this.color);
       ellipseMode(RADIUS)
       ellipse(0, 0, this.radius,this.radius);
       pop();
